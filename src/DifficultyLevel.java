e9e8db3ae4f27f82141c1a3703d9fd62eb3e7257
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2025-06-02 23:20:17 +0700

    update code game

---
+++ src/DifficultyLevel.java
@@ -0,0 +1,16 @@
+public enum DifficultyLevel {
+    EASY(45),
+    MEDIUM(35),
+    HARD(25),
+    EXPERT(17);
+
+    private final int cellsToFill;
+
+    DifficultyLevel(int cellsToFill) {
+        this.cellsToFill = cellsToFill;
+    }
+
+    public int getCellsToFill() {
+        return cellsToFill;
+    }
+}
